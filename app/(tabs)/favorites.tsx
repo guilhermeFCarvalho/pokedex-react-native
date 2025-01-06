ad191a55e3440aaa762e8cdb32d9e665b27f62ae
guilhermeFCarvalho <gui.carvalho021@gmail.com>
2025-01-06 00:58:59 -0300

    feat: add favorites screen

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -1,11 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 
-import { Text, View } from '@/components/Themed';
+import { Text, View, } from '@/components/Themed';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/reducers/store';
 
 export default function FavoritesScreen() {
+
+  const pokemons = useSelector(
+    (state: RootState) => state.favorites.favorites
+  );
   return (
     <View style={styles.container}>
-      
+      {pokemons.map((pokemon, index) => {
+
+        console.log(pokemon.image)
+        
+        return (
+        <View key={index}>
+          <Image source={{ uri: pokemon.image }} width={100} height={100}></Image>
+          <Text >{pokemon.name}</Text>
+          <Text >{pokemon.types.join(' ')}</Text>
+          <Text >{pokemon.abilities.join(' ')}</Text>
+          
+        </View>
+      )})}
+
     </View>
   );
 }
--- reducers/favorite-slice.ts
+++ reducers/favorite-slice.ts
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface Pokemon {
     name: string;
-    url: string;
+    image: string;
     types: string[];
     abilities: string[];
 }
